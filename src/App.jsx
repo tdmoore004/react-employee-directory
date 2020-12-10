de9d872efb12a58061adcbdbb48e21f0e2306d90
tdmoore004 <tdmoore004@gmail.com>
2020-12-09 21:49:25 -0700

    Added functionality to filter users.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { Data } from "./data.js";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
+            display: "normal",
             sort: "",
             filter: ""
         };
@@ -15,60 +16,135 @@ class App extends Component {
         this.handleSort = this.handleSort.bind(this);
         this.handleFilterChange = this.handleFilterChange.bind(this);
         this.handleFilter = this.handleFilter.bind(this);
+        this.sortCompare = this.sortCompare.bind(this);
+        this.filterPosition = this.filterPosition.bind(this);
     }
 
     handleSortChange(event) {
-        this.setState({ sort: event.target.value });
+        this.setState({
+            sort: event.target.value,
+        });
     }
 
     handleSort(event) {
         event.preventDefault();
+        this.setState({
+            display: "sort"
+        });
         console.log(this.state.sort);
     }
 
     handleFilterChange(event) {
-        this.setState({ filter: event.target.value });
+        this.setState({
+            filter: event.target.value,
+        });
     }
 
     handleFilter(event) {
         event.preventDefault();
+        this.setState({
+            filter: event.target.value,
+            display: "filter"
+        });
         console.log(this.state.filter);
     }
 
-    render() {
-        console.log("Rendered!");
-        return (
-            <div>
+    sortCompare(user1, user2) {
+        if (this.state.sort === "Name") {
+            const nameA = user1.name.toUpperCase();
+            const nameB = user2.name.toUpperCase();
 
-                <Header header="React Employee Directory" />
+            let comparison = 0;
+            if (nameA > nameB) {
+                comparison = 1;
+            } else if (nameA < nameB) {
+                comparison = -1;
+            }
+            return comparison;
+        } else if (this.state.sort === "Title") {
+            const nameA = user1.title.toUpperCase();
+            const nameB = user2.title.toUpperCase();
 
-                <form onSubmit={this.handleSort}>
-                    <label>
-                        <select value={this.state.sort} onChange={this.handleSortChange}>
-                            <option value="" selected>Sort By...</option>
-                            <option value="Name">Name</option>
-                            <option value="Title">Title</option>
-                            <option value="Location">Location</option>
-                        </select>
-                    </label>
-                    <input type="submit" value="Sort" />
-                </form>
+            let comparison = 0;
+            if (nameA > nameB) {
+                comparison = 1;
+            } else if (nameA < nameB) {
+                comparison = -1;
+            }
+            return comparison;
+        } else if (this.state.sort === "Location") {
+            const nameA = user1.location.toUpperCase();
+            const nameB = user2.location.toUpperCase();
+          
+            let comparison = 0;
+            if (nameA > nameB) {
+              comparison = 1;
+            } else if (nameA < nameB) {
+              comparison = -1;
+            }
+            return comparison;
+        } else {
+            this.setState({
+                display: "normal"
+            })
+        }
+    }
 
-                <form onSubmit={this.handleFilter}>
-                    <label>
-                        <select value={this.state.filter} onChange={this.handleFilterChange}>
-                            <option value="" selected>Filter by Position...</option>
-                            <option value="Manager">Manager</option>
-                            <option value="Front-End Developer">Front-End Developer</option>
-                            <option value="Back-End Developer">Back-End Developer</option>
-                            <option value="UX/UI Designer">UX/UI Designer</option>
-                            <option value="Graphic Designer">Graphic Designer</option>
-                            <option value="Intern">Intern</option>
-                        </select>
-                    </label>
-                    <input type="submit" value="Filter" />
-                </form>
+    filterPosition(user) {
+        if (this.state.filter === "Manager") {
+            return user.title === "Manager"
+        } else if (this.state.filter === "Front-End Developer") {
+            return user.title === "Front-End Developer"
+        } else if (this.state.filter === "Back-End Developer") {
+            return user.title === "Back-End Developer"
+        } else if (this.state.filter === "UX/UI Designer") {
+            return user.title === "UX/UI Designer"
+        } else if (this.state.filter === "Graphic Designer") {
+            return user.title === "Graphic Designer"
+        } else if (this.state.filter === "Intern") {
+            return user.title === "Intern"
+        } else {
+            this.setState({
+                display: "normal"
+            })
+        }
+    }
 
+render() {
+    console.log("Rendered!");
+    return (
+        <div>
+
+            <Header header="React Employee Directory" />
+
+            <form onSubmit={this.handleSort}>
+                <label>
+                    <select value={this.state.sort} onChange={this.handleSortChange}>
+                        <option value="" selected>Sort By...</option>
+                        <option value="Name">Name</option>
+                        <option value="Title">Title</option>
+                        <option value="Location">Location</option>
+                    </select>
+                </label>
+                <input type="submit" value="Sort" />
+            </form>
+
+            <form onSubmit={this.handleFilter}>
+                <label>
+                    <select value={this.state.filter} onChange={this.handleFilterChange}>
+                        <option value="" selected>Filter by Position...</option>
+                        <option value="Manager">Manager</option>
+                        <option value="Front-End Developer">Front-End Developer</option>
+                        <option value="Back-End Developer">Back-End Developer</option>
+                        <option value="UX/UI Designer">UX/UI Designer</option>
+                        <option value="Graphic Designer">Graphic Designer</option>
+                        <option value="Intern">Intern</option>
+                    </select>
+                </label>
+                <input type="submit" value="Filter" />
+            </form>
+
+            {this.state.display === "normal" && (
                 <ul>
                     {Data.map((user) => {
                         return (
@@ -83,10 +159,44 @@ class App extends Component {
                             </li>)
                     })}
                 </ul>
+            )}
 
-            </div>
-        );
-    }
+            {this.state.display === "sort" && (
+                <ul>
+                    {Data.sort(this.sortCompare).map((user) => {
+                        return (
+                            <li>
+                                {user.name}
+                                <ul>
+                                    <li>Title: {user.title}</li>
+                                    <li>Email: {user.email}</li>
+                                    <li>Phone: {user.phone}</li>
+                                    <li>City: {user.location}</li>
+                                </ul>
+                            </li>)
+                    })}
+                </ul>
+            )}
+
+            {this.state.display === "filter" && (
+                <ul>
+                    {Data.filter(this.filterPosition).map((user) => {
+                        return (
+                            <li>
+                                {user.name}
+                                <ul>
+                                    <li>Title: {user.title}</li>
+                                    <li>Email: {user.email}</li>
+                                    <li>Phone: {user.phone}</li>
+                                    <li>City: {user.location}</li>
+                                </ul>
+                            </li>)
+                    })}
+                </ul>
+            )}
+        </div>
+    );
+}
 }
 
 export default App;
